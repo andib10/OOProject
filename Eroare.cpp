59accd17016785ec7d9b7f338374b7af4d2a765f
andib10 <81637678+andib10@users.noreply.github.com>
2021-11-23 17:35:54 +0200

    add header

--- Eroare.cpp
+++ Eroare.cpp
@@ -1,6 +1,7 @@
 
 #include "Eroare.h"
+#include <string>
 
-Eroare::Eroare(const std::string &arg) : runtime_error(arg) {}
+Eroare::Eroare(const std::string &arg) : runtime_error("Eroare ingredient: " + arg) {}
 
 ingredient_not_found::ingredient_not_found() : Eroare("ingredientul nu a fost gasit") {}
